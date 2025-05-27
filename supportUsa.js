e42390a4289bf7149262466ed83d4af7ba0662e7
OPELMOARRRRR <168326091+faggggg123459@users.noreply.github.com>
2025-05-27 16:10:07 +0300

    Update supportUsa.js

--- supportUsa.js
+++ supportUsa.js
@@ -1,1 +1,13 @@
+function sendMessage(event) {
+  event.preventDefault();
+  const msg = document.getElementById("responseMsg");
+  msg.textContent = "✅ Thank you! Your message has been sent.";
+  msg.style.color = "green";
+}
 
+function submitComplaint(event) {
+  event.preventDefault();
+  const msg = document.getElementById("complaintMsg");
+  msg.textContent = "✅ Your complaint has been successfully registered.";
+  msg.style.color = "darkred";
+}
